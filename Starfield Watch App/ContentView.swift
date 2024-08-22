a42b3406ccd2ede3a9cb55f9840950293187b070
Josi-create <54358116+Josi-create@users.noreply.github.com>
2024-08-22 14:56:18 +0200

    Initial Commit

---
+++ Starfield Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Starfield Watch App
+//
+//  Created by Johannes Wack on 22.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Starfield Watch App/StarfieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarfieldApp.swift
+//  Starfield Watch App
+//
+//  Created by Johannes Wack on 22.08.24.
+//
+
+import SwiftUI
+
+@main
+struct Starfield_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
